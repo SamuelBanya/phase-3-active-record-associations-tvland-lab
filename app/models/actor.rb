a06a462c71292ebcc6d04e753d95c2d544f805e4
Samuel Banya <sbanya@fastmail.com>
2022-09-03 14:10:49 -0400

    commit tree 5f052d6c183a4347af522ac488adeb765c4a58eb

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,31 @@
 class Actor < ActiveRecord::Base
-  
+  # #characters: an actor has many characters, so write code that creates an instance method
+  # that will return all the characters that the actor has played.
+  # Hint: is there an association macro you can use to create this method?
+  has_many :characters
+  # #shows: an actor should have many characters and many shows through characters.
+  # Write code that creates an instance method that returns all the shows for the actor.
+  # Hint: is there an association macro you can use to create this method?
+  has_many :shows, through: :characters
+
+  # #full_name: Write a method in the Actor class, #full_name, that returns the first and last name of an actor.
+  def full_name()
+    # First Attempt:
+    # full_name = self.first_name + " " + self.last_name
+    # full_name
+    # More elegant solution:
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  # #list_roles: Write a method in the Actor class, #list_roles, that lists all of the characters
+  # that actor has alongside the show that the character is in.
+  def list_roles()
+    # First attempt:
+    # self.characters.all().each do |character|
+    # "#{character.name} - #{character.show}"
+    # end
+    self.characters.map() do |character|
+      "#{character.name} - #{character.show.name}"
+    end
+  end
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,24 @@
 class Character < ActiveRecord::Base
-  
+  # #actor: should return the actor instance this character belongs to.
+  # Hint: is there an association macro you can use to create this method?
+  # NOTE: By using 'belongs_to :actor', this will create a '#actor' method
+  # that associates the 'characters' table to the 'actors' table
+  # NOTE: Not to be confusing but we are associating the 'Character' class
+  # to the 'Actor' class hence the singular usage here of the ':actor' symbol:
+  belongs_to :actor
+  #show: should return the show instance this character belongs to.
+  # Hint: is there an association macro you can use to create this method?
+  belongs_to :show
+  #say_that_thing_you_say:
+  # Define a method in the Character class, #say_that_thing_you_say, using a given character's catchphrase.
+  # Using Tyrion as an example again, the returned string should look like the following
+  # tyrion.catchphrase = 'A Lannister always pays his debts'
+  # tyrion.say_that_thing_you_say
+  # => 'Tyrion Lannister always says: A Lannister always pays his debts'
+  def say_that_thing_you_say()
+    # First Attempt:
+    # puts catchphrase
+    # NOTE: Would have never figured this out, terrible hint -->
+    "#{self.name} always says: #{self.catchphrase}"
+  end
 end
